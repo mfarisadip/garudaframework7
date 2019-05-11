b39601d2665d92d790680f41f1c1018681d7b00f
Muhammad Faris Adi Prabowo <40176160+mfaris16@users.noreply.github.com>
2019-05-11 15:10:59 +0700

    Update welcome.garuda.php
    New HomePage

--- app/view/welcome.garuda.php
+++ app/view/welcome.garuda.php
@@ -1,91 +1,81 @@
-[[ defined('sys_run_app') OR exit('403 - Access Forbidden'); ]]
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="og:image" 			content="" />
-	<meta property="og:image"     	content="" />
-	<link rel="shortcut icon" type="image/png" href="icon.png"/>
-	<meta name="ROBOTS" 			content="index"/>
-	<meta name="Author" 			content="@mfaris16" />
-	<meta name="copyright" 			content="Copyright@2018 | All Right Reserved" />
-	<meta property="og:title" 		content="" />
-	<meta property="og:description" content="" />
-	<meta property="og:name"      	content="" />
-	<meta name="language" 		  	content="Indonesian, English" />
-	<meta name="distribution" 	  	content="Global" />
-	<meta name="rating" 		  	content="General" />
-	<meta name="expires" 		  	content="1800" />
-	<meta name="theme-color"      	content="#E43539"> 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Selamat Datang di Garuda Framework 7.0</title>
-	{{ _importBootstrap4(); }}
-	<style>
-		html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-	</style>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>
+    Garuda Framework
+  </title>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link href="http://localhost/GF/app/view/asset/blk/css/blk-design-system.css?v=1.0.0" rel="stylesheet" />
+  <link href="http://localhost/GF/app/view/asset/blk/css/nucleo-icons.css" rel="stylesheet" />
 </head>
-<body>
-		
-	<div class="flex-center position-ref full-height">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Garuda
-                </div>
-                [[
+<body class="index-page">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg fixed-top navbar-transparent" color-on-scroll="100">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="" rel="tooltip" title="Garuda Framework" data-placement="bottom" target="_blank">
+          <span style="color:black">Garuda Framework</span>
+        </a>
+      </div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link btn btn-danger d-none d-lg-block" href="http://dokumentasi.mfaris16.com/" onclick="scrollToDownload()">
+              <i class="tim-icons icon-paper"></i> Documentation
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-danger d-none d-lg-block" href="jhttps://github.com/mfaris16/" onclick="scrollToDownload()">
+              <i class="fab fa-github"></i> Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <div class="wrapper">
+    <div class="page-header header-filter">
+      <div class="squares square1"></div>
+      <div class="squares square2"></div>
+      <div class="squares square3"></div>
+      <div class="squares square4"></div>
+      <div class="squares square5"></div>
+      <div class="squares square6"></div>
+      <div class="squares square7"></div>
+      <div class="container">
+        <div class="content-center brand">
+          <h1 style="color:black; font-size:50px;">Garuda Framework</h1>
+          <span style="color:black">[[
 				$end    = _startMicroTime();
 				$result = $end - __Time_Start__;
 
 				echo " Rendering Page  : <strong>".$result."</strong> seconds";
 				
-				]]
-            </div>
+				]]</span>
         </div>
+      </div>
+    </div>
+  </div>
+  <script>
+    $(document).ready(function() {
+      blackKit.initDatePicker();
+      blackKit.initSliders();
+    });
 
+    function scrollToDownload() {
+
+      if ($('.section-download').length != 0) {
+        $("html, body").animate({
+          scrollTop: $('.section-download').offset().top
+        }, 1000);
+      }
+    }
+  </script>
 </body>
 
 </html>
